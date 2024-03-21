41810a960b0de7b0cdf0f20af626a2e860f0dae3
RahulBirgonda <rahulsrikanth9247@gmail.com>
2024-03-21 23:34:08 +0530

    feat: frontend and database setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,20 @@
 import express, {Request, Response} from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from 'mongoose'
+
+// mongoose.connect(process.env.URL as string)
+mongoose.connect(process.env.URL as string)
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 const app=express()
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use(cors())
 
 app.get("/test",async (req:Request, res:Response)=>{
