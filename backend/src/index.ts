0f3400719822441211da7c1cdd78c82417f530c5
RahulBirgonda <rahulsrikanth9247@gmail.com>
2024-03-23 00:23:55 +0530

    feat: added layout ui

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,26 +1,27 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 // mongoose.connect(process.env.URL as string)
-mongoose.connect(process.env.URL as string)
+mongoose
+  .connect(process.env.URL as string)
   .then(() => {
-    console.log('MongoDB connected successfully');
+    console.log("MongoDB connected successfully");
   })
   .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   });
 
-const app=express()
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.get("/test",async (req:Request, res:Response)=>{
-res.json({message: "Hello"});
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello" });
 });
 
-app.listen(3000,()=>{
-    console.log("Server started on port 3000...")
-})
+app.listen(3000, () => {
+  console.log("Server started on port 3000...");
+});
