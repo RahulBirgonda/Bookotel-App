46553419f68b99a98caae4a733c1a16d3dfa4121
RahulBirgonda <rahulsrikanth9247@gmail.com>
2024-03-24 00:40:29 +0530

    feat(authentication): added login functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 
 // mongoose.connect(process.env.URL as string)
 mongoose
@@ -18,6 +19,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use("/api/auth", authRoutes);
 
 app.use("/api/users", userRoutes);
 
