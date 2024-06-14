e2390182ffd437ab66307c72c3b6faf2e4cc79ec
RahulBirgonda <rahulsrikanth9247@gmail.com>
2024-06-14 09:24:22 +0530

    build fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { AppContextProvider } from "./contexts/AppContext.tsx";
-import { SearchContextProvider } from "./contexts/SearchContext.tsx";
+import { SearchContextProvider } from "./contexts/SearchContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
